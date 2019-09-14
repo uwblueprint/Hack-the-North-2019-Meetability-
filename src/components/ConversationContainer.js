9d80b82073853967ad393e569441618b5e899fd0
Santos Gagbegnon <santos.gagbegnon@shopify.com>
2019-09-14 19:32:12 -0400

    Display data from db

--- src/components/ConversationContainer.js
+++ src/components/ConversationContainer.js
@@ -1,12 +1,16 @@
 //ConversationContainer.js
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 //import { } from '@reach/router';
-//import { useDispatch, useSelector } from 'react-redux';
-//import { } from '../redux/selectors';
+import { useSelector } from 'react-redux';
+import { getAllUsers } from '../redux/selectors';
+import { getMessages } from '../redux/selectors/thread'
+
 //import { } from '../redux/actions';
 //import { makeStyles } from '@material-ui/core/styles';
-// import Typography from '@material-ui/core/Typography';
-import MessageContainer from '../components/MessageContainer';
+import Typography from '@material-ui/core/Typography';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+
+// import MessageContainer from '../components/MessageContainer';
 /*
 const useStyles = makeStyles(theme => ({
     root: {
@@ -19,19 +23,24 @@ export default function ConversationContainer() {
 
     //const dispatch = useDispatch();
     //const classes = useStyles();
-    const data = useState(["More messages", "Even more", "Even more"])[0]
-    const messages = data.map((message, i) => {
+    const all_users = useSelector(getAllUsers)
+    // const thread = useSelector(getThread);
+    const messages = useSelector(getMessages);
+    const messageContainers = messages.map((message, i) => {
         return (
-          <MessageContainer
-            key={i}
-            message={message.message}/>
+            <div style={{margin:"0px",padding:"0px 100px 10px 10px", textAlign:"left"}}>
+                <Typography style={{marginLeft: "12px",marginBottom:"-10px" ,fontSize: "12px", color:"gray"}}>
+                    {all_users[message.from].username}
+                </Typography>
+                <SnackbarContent message={message.content} style={{width:"20px", margin: "10px"}}></SnackbarContent>
+            </div>
         );
       });
 
     return (
         <Fragment>
             <div className='messages' id='messageList'>
-                { messages }
+                { messageContainers }
             </div>
         </Fragment>
     );
