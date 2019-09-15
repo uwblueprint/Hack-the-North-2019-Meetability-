82ba39d1b0fd892b19612556811a7d1b663913b8
Santos Gagbegnon <santos.gagbegnon@shopify.com>
2019-09-14 20:46:24 -0400

    Add scrolling for messages

--- src/components/ConversationContainer.js
+++ src/components/ConversationContainer.js
@@ -2,7 +2,7 @@
 import React, { Fragment } from 'react';
 //import { } from '@reach/router';
 import { useSelector } from 'react-redux';
-import { getAllUsers } from '../redux/selectors';
+import { getAllUsers, getUser } from '../redux/selectors';
 import { getMessages } from '../redux/selectors/thread'
 
 //import { } from '../redux/actions';
@@ -24,13 +24,17 @@ export default function ConversationContainer() {
     //const dispatch = useDispatch();
     //const classes = useStyles();
     const all_users = useSelector(getAllUsers)
+    const user = useSelector(getUser)
     // const thread = useSelector(getThread);
     const messages = useSelector(getMessages);
     const messageContainers = messages.map((message, i) => {
+        const messagePadding = all_users[message.from].username == user.username ? "0px 10px 10px 930px" : "0px 10px 10px 10px"
+        const messageFrom = all_users[message.from].username == user.username ? "You" : all_users[message.from].username
+        console.log(messagePadding)
         return (
-            <div style={{margin:"0px",padding:"0px 100px 10px 10px", textAlign:"left"}}>
+            <div style={{margin:"0px",padding: messagePadding, textAlign:"left"}}>
                 <Typography style={{marginLeft: "12px",marginBottom:"-10px" ,fontSize: "12px", color:"gray"}}>
-                    {all_users[message.from].username}
+                    {messageFrom}
                 </Typography>
                 <SnackbarContent message={message.content} style={{width:"20px", margin: "10px"}}></SnackbarContent>
             </div>
@@ -39,7 +43,7 @@ export default function ConversationContainer() {
 
     return (
         <Fragment>
-            <div className='messages' id='messageList'>
+            <div className='messages' id='messageList' style={{height:"90vh", overflow:"scroll"}}>
                 { messageContainers }
             </div>
         </Fragment>
